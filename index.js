47e2f73f00d9e6b96ce8401d385f947163277a6a
WHITELOTUS <126754757+WHITELOTUS4@users.noreply.github.com>
2025-03-11 15:44:22 +0530

    Version 1.0

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const socketIo = require('socket.io');
+const socketIo = require('./modules/socket.io');
 const path = require('path');
 const ejs = require('ejs');
 require('dotenv').config();
